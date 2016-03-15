c53d8f9390b7cc5af3e942438b2623c541434834
Ángel Luis López Monterroso <angel9484@gmail.com>
2016-03-15 07:40:23 +0100

    changed the place of the webservice application example source code to a folder called examples/webservice/java and removed from README

---
+++ examples/webservice/java/src/main/java/es/nangel/logstash/webservicemap/test/Test.java
@@ -0,0 +1,11 @@
+package es.nangel.logstash.webservicemap.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test {
+    public static void main(String[] args) {
+        SpringApplication.run(Test.class, args);
+    }
+}
